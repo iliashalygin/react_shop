f2ac177bb26baaf79276ea89e28751af485efccf
Ilya <il57@yandex.ru>
2023-01-10 21:50:59 +0300

    Added live search

--- src/components/catalog/Catalog.jsx
+++ src/components/catalog/Catalog.jsx
@@ -1,33 +1,49 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import s from "./Catalog.module.css"
+import search from "../slider/img/search.png"
 
 const Catalog = (props) => {
-  const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
   useEffect(() => {
-    setLoading(true);
     axios({
       method: "GET",
       url: "https://fakestoreapi.com/products"
     }).then(res => {
-      console.log(res.data);
       setData(res.data)
-    }).catch(e => console.log(e))
-      .finally(() => setLoading(false))
+    })
   }, [])
+
+  const [value, setValue] = useState('')
+  const searchCatalog = data.filter(product => {
+    return product.title.toLowerCase().includes(value.toLowerCase())
+  })
+
   return (
-    <div className={s.products_container}>
-      {data.map((product) => (
-        <div key={product.id} className={s.card}>
-          <div><img src={product.image} className={s.imgSlider} /></div>
-          <div className={s.card_description}>
-            <h6 className={s.titleSlider}>{product.title}</h6>
-            <h6>{`Цена: ${product.price} $`}</h6>
-            <h6>{`Категория: ${product.category}`}</h6>
+    <div>
+      <div className={s.form}>
+        <form className={s.search__form} >
+          <input
+            type="text"
+            placeholder="Поиск"
+            className={s.search__input}
+            onChange={(event) => setValue(event.target.value)}
+          />
+          <img src={search} className={s.search__img} />
+        </form>
+      </div>
+      <div className={s.products_container}>
+        {searchCatalog.map((product) => (
+          <div key={product.id} className={s.card}>
+            <div><img src={product.image} className={s.imgSlider} /></div>
+            <div className={s.card_description}>
+              <h6 className={s.titleSlider}>{product.title}</h6>
+              <h6>{`Цена: ${product.price} $`}</h6>
+              <h6>{`Категория: ${product.category}`}</h6>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   )
 }
